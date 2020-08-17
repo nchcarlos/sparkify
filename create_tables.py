3d14f4845583950abfbc7487e85133c692c87273
Chuck Francis <cfrancisjr@hotmail.com>
2020-08-17 18:20:50 -0400

    Update .gitignore and stub project files

---
+++ create_tables.py
@@ -0,0 +1,17 @@
+import psycopg2
+from sql_queries import create_table_queries, drop_table_queries
+
+def create_database():
+    pass
+
+def drop_tables(cur, conn):
+    pass
+
+def create_tables(cur, conn):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ etl.py
@@ -0,0 +1,21 @@
+import os
+import glob
+import psycopg2
+import pandas as pd
+from sql_queries import *
+
+
+def process_song_file(cur, filepath):
+    pass
+
+def process_log_file(cur, filepath):
+    pass
+
+def process_data(cur, conn, filepath, func):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
